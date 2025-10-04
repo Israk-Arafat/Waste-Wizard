892b489f337bdac28853769e2f302ae4278943cc
Juhayer6437 <juhayerislam.69@gmail.com>
2025-10-03 21:38:00 -0400

    nav bar

--- src/home.js
+++ src/home.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './home.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar">
+        <div className="navbar-brand">WasteWizard</div>
+      </nav>
+      <div className="content">
+        {/* Your main content will go here */}
+      </div>
     </div>
   );
 }
